931bed295369a44db11f691d8dbdeaf5f1bd16d9
jimmypuli <125110493+jimmypuli@users.noreply.github.com>
2023-02-10 23:44:20 -0500

    Initial commit

---
+++ settings.py
@@ -0,0 +1,14 @@
+class Settings: 
+	def __init__(self): 
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (0,0,0)
+		self.ship_speed = 0.5
+		self.alien_speed = 0.85
+		self.fleet_drop_speed = 10
+		self.fleet_direction = 1
+		self.bullet_speed = 0.5
+		self.bullet_width = 5
+		self.bullet_height = 20 
+		self.bullet_color = (60,60,60)
+		self.bullets_allowed = 4
